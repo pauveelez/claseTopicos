f9a54ce72a942f64370d06267c87b8a898c21477
pauveelez <paovelezrincon@gmail.com>
2013-10-25 21:05:34 -0500

    admin y fondo
    Administrador
    mas background

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from .models import Estudiante, Carrera
 
-admin.site.register(Estudiante)
-admin.site.register(Carrera)
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'imagen_rosada', 'email', 'telefono', 'carrera')
+    list_editable = ('email', 'telefono', 'carrera', )
+    #list_display_links = ('carrera',)
+    list_filter = ('carrera',)
+    search_fields = ('carrera__nombre', 'nombre')
+
+    raw_id_fields = ('carrera',)
+
+    def imagen_rosada(self, obj):
+        url = obj.dar_imagen_nombre_rosada()
+        tag = '<img src="%s"/>' % url
+        return tag
+    imagen_rosada.allow_tags = True
+    imagen_rosada.admin_order_field = 'carrera'
+
+class EstudianteInline(admin.StackedInline):
+    model = Estudiante
+    extra = 1
+
+class CarreraAdmin(admin.ModelAdmin):
+    inlines = [EstudianteInline]
+
+admin.site.register(Estudiante, EstudianteAdmin)
+admin.site.register(Carrera, CarreraAdmin)
--- app/models.py
+++ app/models.py
@@ -14,5 +14,14 @@ class Estudiante(models.Model):
     telefono = models.CharField(max_length = 10, null= True)
     carrera = models.ForeignKey(Carrera, null=True)
 
+    def dar_imagen_nombre_rosada(self):
+        return 'http://placehold.it/200x100/E8117F/ffffff/&text=%s' % self.nombre
+
+    def es_informatico(self):
+        return self.carrera == 'Ingenieria Informatica'
+
+    es_informatico.boolean = True
+    es_informatico.admin_order_field = 'carrera'
+
     def __unicode__(self):
         return '%s: %s : %s' % (self.nombre, self.email, self.carrera)
